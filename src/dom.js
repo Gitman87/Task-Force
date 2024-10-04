742316b9a2631e300345e32ba3b117281086747b
Gitman87 <200~83942039+Gitman87@users.noreply.github.com>
2024-10-04 19:49:54 +0200

    Add open/close dialog function , add add listener function

--- src/dom.js
+++ src/dom.js
@@ -1,14 +1,54 @@
 const addProjectBtn = document.querySelector("#add-project");
+const addProjectDialog=document.querySelector("#add-project-dialog");
+const projectTitleInput=document.querySelector("#project-title-input");
 
 const  addAction = (selector, event = "click", action) => {
   const elements = document.querySelectorAll(selector);
+  const eventString = event.toString();
   elements.forEach((element) => {
-    element.addEventListener(event, () => action);
+    element.addEventListener(eventString, () => action);
   });
 }
+const changeElementAttribute =  (element, attribute)=>{
+  
+  if(!element.hasAttribute(attribute)){
+    element.setAttribute(attribute, attribute)
+}
+  else{
+    element.removeAttribute(attribute)
+  }
+}
+
+
+addAction(addProjectBtn, changeElementAttribute(addProjectDialog, "open"));
+
+const submitEnter = (input)=>{
+  input.addEventListener('keypress', (event)=>{
+    if(event.key === "Enter"){
+      event.preventDefault();
+      return input.value;
+    }
+  })
+
+
+}
+
+
+
 // const showWindow = 
 // adding new project
 
-const projectPanel = new ProjectPanel;
+// const projectPanel = new ProjectPanel;
 
- addAction("#add-project", projectPanel.projectManager.addProject(title));
+// const openCloseDialog = (element)=>{
+//   if (!element.hasAttribute('open')) {
+//     element.setAttribute('open', 'open');
+//   }
+//   else{
+//     element.removeAttribute('open');
+//   }
+// }
+
+
+//open dialog box with form to add project
+
